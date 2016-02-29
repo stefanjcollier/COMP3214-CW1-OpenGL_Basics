206203b4c7991656f39aa656a226442dc8aed1d0
Stefan Collier <sc22g13@soton.ac.uk>
2016-02-29 12:05:08 +0000

    Added the Cone class, but now has errors

--- Cone.h
+++ Cone.h
@@ -16,24 +16,58 @@ public:
 
 	void populateArrayWithCone(GLfloat* arr, GLuint lineNo, GLfloat radius, GLfloat height) {
 		GLuint offset = lineNo * vertIndxs;
-		GLuint j = 0;
+		
+		//Save the first circle point
+		generatingNextCirclePoint(radius);
+		GLfloat ox = x, oy = y, oz = z;
+
+		//Create the top point
 		GLfloat tx = 0.0f, ty = 0.0f, tz = height;
+
+		// The index in the data array
+		GLuint j = 0;
+
+		//circle node n_0
+		arr[offset + j*vertIndxs + 0] = ox;
+		arr[offset + j*vertIndxs + 1] = oy;
+		arr[offset + j*vertIndxs + 2] = oz;
+		j++;
+
+		//cone top node
+		arr[offset + j*vertIndxs + 0] = tx;
+		arr[offset + j*vertIndxs + 1] = ty;
+		arr[offset + j*vertIndxs + 2] = tz;
+		j++;
+
+
+		//Create all intermediary triangles
 		while (generatingNextCirclePoint(radius)) {
-			arr[offset+j*vertIndxs + 0] + x;
-			arr[offset+j*vertIndxs + 1] + y;
-			arr[offset+j*vertIndxs + 2] + z;
+
+			//circle node n_i for the end of  the previous triangle
+			arr[offset+j*vertIndxs + 0] = x;
+			arr[offset+j*vertIndxs + 1] = y;
+			arr[offset+j*vertIndxs + 2] = z;
 			j++;
 
-			arr[offset+j*vertIndxs + 0] + tx;
-			arr[offset+j*vertIndxs + 1] + ty;
-			arr[offset+j*vertIndxs + 2] + tz;
+			// circle node n_i for the  start of this triangle
+			arr[offset + j*vertIndxs + 0] = x;
+			arr[offset + j*vertIndxs + 1] = y;
+			arr[offset + j*vertIndxs + 2] = z;
 			j++;
 
-			arr[offset+j*vertIndxs + 0] + x;
-			arr[offset+j*vertIndxs + 1] + y;
-			arr[offset+j*vertIndxs + 2] + z;
+
+			//cone top node
+			arr[offset+j*vertIndxs + 0] = tx;
+			arr[offset+j*vertIndxs + 1] = ty;
+			arr[offset+j*vertIndxs + 2] = tz;
 			j++;
 		}
+
+		//Finishing of the very last triangle with the circle origin node
+		arr[offset + j*vertIndxs + 0] = ox;
+		arr[offset + j*vertIndxs + 1] = oy;
+		arr[offset + j*vertIndxs + 2] = oz;
+		j++;
 	}
 
 	void drawCone(GLuint offset) {
@@ -56,5 +90,4 @@ private:
 			return i > 2 * nodesInDemiCircle;
 			//The other bits are not my concern atm
 		}
-	}
 };
